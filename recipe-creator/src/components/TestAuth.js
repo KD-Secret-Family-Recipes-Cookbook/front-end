74071d465c5a83084fe44504cf94d2e90214a550
taterntots <matthew.a.weidner@gmail.com>
2019-11-20 15:53:48 -0500

    messing around with axios and my get command. not resolved.

--- recipe-creator/src/components/TestAuth.js
+++ recipe-creator/src/components/TestAuth.js
@@ -6,7 +6,7 @@ export const axiosWithAuth = () => {
         baseURL: 'https://secretfamilyrecipescookbook.herokuapp.com',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': `Bearer 173907f6-f085-4453-8d00-9decf80d2687`,
+            'Authorization': `${token}`,
         },
     });
 };
